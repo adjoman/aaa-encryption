f9e9731171fadeb0cfe2f77708701b8d02aad145
adjo <calvin@kairuconsulting.com>
2018-12-03 12:21:01 -0800

    sample code for encryption

---
+++ Helper/CommonFunctions.cs
@@ -0,0 +1,113 @@
+﻿
+using System;
+using System.IO;
+using System.Security.Cryptography;
+
+namespace AAA.Helpers
+{
+    public static class CommonFunctions
+    {
+        public static string FormatNumber(int num)
+        {
+            if (num >= 100000)
+            {
+                return FormatNumber(num / 1000) + "K";
+            }
+
+            if (num >= 10000)
+            {
+                return (num / 1000D).ToString("0.#") + "K";
+            }
+
+            if (num >= 1000)
+            {
+                return (num / 1000D).ToString("0.#") + "K";
+            }
+
+            return num.ToString("#,0");
+        }
+
+        private static byte[] CreateKey(string password, int keyBytes = 32)
+        {
+            byte[] salt = new byte[] { 80, 70, 60, 50, 40, 30, 20, 10 };
+            int iterations = 300;
+            var keyGenerator = new Rfc2898DeriveBytes(password, salt, iterations);
+            return keyGenerator.GetBytes(keyBytes);
+        }
+
+        public static string Encrypt(this string clearValue, string encryptionKey)
+        {
+            using (Aes aes = Aes.Create())
+            {
+                aes.Key = CreateKey(encryptionKey);
+
+                byte[] encrypted = AesEncryptStringToBytes(clearValue, aes.Key, aes.IV);
+                return Convert.ToBase64String(encrypted) + ";" + Convert.ToBase64String(aes.IV);
+            }
+        }
+
+        private static byte[] AesEncryptStringToBytes(string plainText, byte[] key, byte[] iv)
+        {
+            if (plainText == null || plainText.Length <= 0)
+                throw new ArgumentNullException($"{nameof(plainText)}");
+            if (key == null || key.Length <= 0)
+                throw new ArgumentNullException($"{nameof(key)}");
+            if (iv == null || iv.Length <= 0)
+                throw new ArgumentNullException($"{nameof(iv)}");
+
+            byte[] encrypted;
+
+            using (Aes aes = Aes.Create())
+            {
+                aes.Key = key;
+                aes.IV = iv;
+
+                using (MemoryStream memoryStream = new MemoryStream())
+                {
+                    using (ICryptoTransform encryptor = aes.CreateEncryptor())
+                    using (CryptoStream cryptoStream = new CryptoStream(memoryStream, encryptor, CryptoStreamMode.Write))
+                    using (StreamWriter streamWriter = new StreamWriter(cryptoStream))
+                    {
+                        streamWriter.Write(plainText);
+                    }
+                    encrypted = memoryStream.ToArray();
+                }
+            }
+            return encrypted;
+        }
+
+        public static string Decrypt(this string encryptedValue, string encryptionKey)
+        {
+            string iv = encryptedValue.Substring(encryptedValue.IndexOf(';') + 1, encryptedValue.Length - encryptedValue.IndexOf(';') - 1);
+            encryptedValue = encryptedValue.Substring(0, encryptedValue.IndexOf(';'));
+
+            return AesDecryptStringFromBytes(Convert.FromBase64String(encryptedValue), CreateKey(encryptionKey), Convert.FromBase64String(iv));
+        }
+
+        private static string AesDecryptStringFromBytes(byte[] cipherText, byte[] key, byte[] iv)
+        {
+            if (cipherText == null || cipherText.Length <= 0)
+                throw new ArgumentNullException($"{nameof(cipherText)}");
+            if (key == null || key.Length <= 0)
+                throw new ArgumentNullException($"{nameof(key)}");
+            if (iv == null || iv.Length <= 0)
+                throw new ArgumentNullException($"{nameof(iv)}");
+
+            string plaintext = null;
+
+            using (Aes aes = Aes.Create())
+            {
+                aes.Key = key;
+                aes.IV = iv;
+
+                using (MemoryStream memoryStream = new MemoryStream(cipherText))
+                using (ICryptoTransform decryptor = aes.CreateDecryptor())
+                using (CryptoStream cryptoStream = new CryptoStream(memoryStream, decryptor, CryptoStreamMode.Read))
+                using (StreamReader streamReader = new StreamReader(cryptoStream))
+                    plaintext = streamReader.ReadToEnd();
+
+            }
+            return plaintext;
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using AAA.Helpers;
 
 namespace AAAEncryptionExample
 {
@@ -6,7 +7,21 @@ namespace AAAEncryptionExample
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            // this key can be anything the company wants
+            string encryptionKey = "99fa50c4-54d3-47c3-afc7-ec1a050a4fd4";
+
+            // what this program does
+            // 1. Grabs the company key -- should be store somewhere safe -- config file, etc
+            // 2. uses that key to encrypt a string -- the string in this example is hello-world
+            // 3. decrypts the key to reveal the plain text string
+
+            string stringToEncrypt = "Hello World";
+            string encryptedString = stringToEncrypt.Encrypt(encryptionKey);
+            string decryptedString = encryptedString.Decrypt(encryptionKey);
+
+            Console.WriteLine("Encrypted Key: " + encryptedString);
+            Console.WriteLine("Decrypting....");
+            Console.WriteLine("Decrypted String: " + decryptedString);
         }
     }
 }
